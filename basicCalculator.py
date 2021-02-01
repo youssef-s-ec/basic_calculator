a0b50ed22e5cdcd5b98455cf5df068a07efbf91f
youssef <youssef.s.ec@gmail.com>
2021-02-01 20:15:13 +0100

    getting inputs from user

---
+++ basicCalculator.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+
+    number1 = float(input(" please enter first number: "))
+
+    operator = input(" please enter operator: ")
+
+    number2 = float(input("please enter second number:"))
