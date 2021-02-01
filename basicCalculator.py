a58c4847ca7784aca625ef58191d91c9d56d4a9f
youssef <youssef.s.ec@gmail.com>
2021-02-01 20:21:50 +0100

    calculator done with 4 basic operator

--- basicCalculator.py
+++ basicCalculator.py
@@ -5,3 +5,14 @@ if __name__ == '__main__':
     operator = input(" please enter operator: ")
 
     number2 = float(input("please enter second number:"))
+
+    if operator == "+":
+        print(number1+number2)
+    elif operator == "-":
+        print(number1-number2)
+    elif operator == "*":
+        print(number1*number2)
+    elif operator == "/":
+        print(number1/number2)
+    else:
+        print("Invalid operator.")
